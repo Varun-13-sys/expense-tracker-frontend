21ef6658d09b532a96ffa86ab7d3fca925d643a3
VARUN A <Varunarya463@gmail.com>
2025-11-22 20:17:09 +0530

    changed the repo name

--- Expense-tracker-frontend/vite.config.js
+++ Expense-tracker-frontend/vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// IMPORTANT: Your repo name EXACTLY
 export default defineConfig({
   plugins: [react()],
-  base: "/expense-tracker-frontend/", 
+  base: "/expense-tracker-frontend/",
 })
